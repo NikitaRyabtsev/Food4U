4e9718688864c96fa915bdc0d9fc5d13cfcd313c
Ryabtsev <RyabtsevNukuta@yandex.by>
2021-11-16 00:36:39 +0300

    #37 fix dishes html

--- src/main/java/by/htp/netcracker/foodfactory/Reposotories/DishRepository.java
+++ src/main/java/by/htp/netcracker/foodfactory/Reposotories/DishRepository.java
@@ -3,6 +3,7 @@ package by.htp.netcracker.foodfactory.Reposotories;
 import by.htp.netcracker.foodfactory.Model.Dish;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.criteria.CriteriaBuilder;
